11fb990ed0897103c7e1197c023395ba8e6ed543
jakewmasters <jakewmasters@gmail.com>
2017-01-19 09:45:11 -0500

    Initial File Attachment
    Attached the html, css, and js files to the master branch.

---
+++ A8.js
@@ -0,0 +1,24 @@
+function createAccount(){
+  var loginInfo = "Name=" + createUser.value +
+	             "&Password=" + createPassword.value;
+  var ajax = new XMLHttpRequest();
+	ajax.responseType = "json";
+	ajax.addEventListener("load", function() {
+		 console.log("server response =", this.response[0]);
+    } );
+	ajax.open('POST', '//cse.taylor.edu/~cos143/users.php');
+	ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	ajax.send(loginInfo);
+}
+
+function login() {
+	var params = "Name=admin&Password=admin";
+	var ajax = new XMLHttpRequest();
+	ajax.responseType = "json";
+	ajax.addEventListener("load", function() { 
+		 console.log("server response =", this.response[0]);
+    } );
+	ajax.open('POST', '//cse.taylor.edu/~cos143/sessions.php');
+	ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	ajax.send(params);
+}
